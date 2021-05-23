531e5ded26a4df85928b3a6e54137024f1389589
Riceball LEE <snowyu.lee@gmail.com>
2021-05-23 20:28:56 +0800

    build(RN): upgrade ReactNative to 0.64.1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { AppRegistry, Platform } from "react-native";
 import App from "./App";
 
 if (Platform.OS === "android") {
-  require("babel-polyfill");
+  require("react-native-get-random-values");
 }
 
 AppRegistry.registerComponent("matchimals", () => App);
